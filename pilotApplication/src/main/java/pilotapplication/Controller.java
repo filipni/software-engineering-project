0f5985a8a66b3cb2593816abffe80c3b24271d2c
Filip Lindahl <gysztaf@gmail.com>
2017-04-25 10:59:27 +0200

    Added a few fields to the GUI and a calculation.

--- pilotApplication/src/main/java/pilotapplication/Controller.java
+++ pilotApplication/src/main/java/pilotapplication/Controller.java
@@ -3,11 +3,15 @@ package pilotapplication;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.jboss.as.controller.client.helpers.standalone.ServerUpdateActionResult.Result;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class Controller implements Initializable
 {
@@ -19,14 +23,27 @@ public class Controller implements Initializable
 	}
 	
 	@FXML
-	private Button button1;
+	private Button calcButton;
 	
 	@FXML
-	private Label label1;
+	private Label distLabel, speedLabel, resultLabel;
+	
+	@FXML
+	private Text resultText;
+	
+	@FXML
+	private TextField distTextField, speedTextField;
 	
 	public void buttonHandler(ActionEvent event)
 	{
-		label1.setText("Well done!");
+		double distance = Double.parseDouble(distTextField.getText());
+		double speed = Double.parseDouble(speedTextField.getText())*1852; //1 knop=1.852km/h = 1852m/h
+		double result = calculateDistance(distance, speed);
+		resultText.setText("Anländer om " + Double.toString(result) + "h");
+	}
+	
+	private double calculateDistance(double distance, double speed){
+		return distance/speed;
 	}
 	
 	
