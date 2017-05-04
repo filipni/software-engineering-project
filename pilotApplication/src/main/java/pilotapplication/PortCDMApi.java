035b4704aa8b8c03f49ef760613519b721be6e36
filipni <filipni@student.chalmers.se>
2017-05-04 23:31:29 +0200

    Config files now integrated into the project.

--- pilotApplication/src/main/java/pilotapplication/PortCDMApi.java
+++ pilotApplication/src/main/java/pilotapplication/PortCDMApi.java
@@ -24,6 +24,7 @@ public class PortCDMApi {
 	private final String DEV_PASSWORD = "vik123"; 
 	private final String DEV_API_KEY = "pilot";
 	private final int DEV_TIMEOUT = 20000;
+	private  final String DEV_CONFIG_FILE_NAME = "dev_portcdm.conf";
 			
 	// Parameters for the local virtual machine
 	private final String VM_BASE_URL = "http://192.168.56.101:8080/";
@@ -31,13 +32,11 @@ public class PortCDMApi {
 	private final String VM_PASSWORD = "porter"; // LOL security
 	private final String VM_API_KEY = "pilot";	
 	private final int VM_TIMEOUT = 7000;
+	private  final String VM_CONFIG_FILE_NAME = "vm_portcdm.conf";
 	
 	// Paths to PortCDMs different modules
 	private final String MESSAGE_BROKER_PATH = "mb";
 	private final String PORT_CDM_SERVICES_PATH = "dmp";
-	
-	public  final String CONFIG_FILE_NAME = "portcdm.conf";
-	public  final String CONFIG_FILE_DIR = "portcdm";
 		
 	public SubmissionService submissionService;
 	public MessageQueueServiceApi messageBrokerAPI;
@@ -50,12 +49,12 @@ public class PortCDMApi {
 	 */
 	public PortCDMApi(boolean connectToExternalServer) {
 		if (connectToExternalServer) {
-			initSubmissionService(CONFIG_FILE_NAME, CONFIG_FILE_DIR);
+			initSubmissionService(DEV_CONFIG_FILE_NAME, null);
 			initMessageBrokerAPI(DEV_BASE_URL + MESSAGE_BROKER_PATH, DEV_TIMEOUT);
 			initPortCallsAPI(DEV_BASE_URL + PORT_CDM_SERVICES_PATH, DEV_TIMEOUT, DEV_USERNAME, DEV_PASSWORD, DEV_API_KEY);
 		}
 		else {
-			initSubmissionService(CONFIG_FILE_NAME, CONFIG_FILE_DIR);
+			initSubmissionService(VM_CONFIG_FILE_NAME, null);
 			initMessageBrokerAPI(VM_BASE_URL + MESSAGE_BROKER_PATH, VM_TIMEOUT);
 			initPortCallsAPI(VM_BASE_URL + PORT_CDM_SERVICES_PATH, VM_TIMEOUT, VM_USERNAME, VM_PASSWORD, VM_API_KEY);
 		}	
