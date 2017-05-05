8449773b726e3c69d40ae4bffc246bc28e7d7ed7
filipni <filipni@student.chalmers.se>
2017-05-05 05:02:55 +0200

    The pop-up window now displays actual information from portCDM.

--- pilotApplication/src/main/java/pilotapplication/Controller.java
+++ pilotApplication/src/main/java/pilotapplication/Controller.java
@@ -1,30 +1,36 @@
 package pilotapplication;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
-
+import eu.portcdm.dto.PortCallSummary;
 import eu.portcdm.messaging.PortCallMessage;
 
 public class Controller implements Initializable {	
 	@FXML
-	private ListView<Button> IDListView; 
+	private ListView<String> idListView; 
 	
 	@FXML
-	private Text idText, inkOrAvgText, bookTimeText, etaTimeText;
+	private Text idText, vesselStatusText, bookTimeText, etaTimeText;
 	
 	@FXML 
 	private GridPane gridPane1; 
@@ -33,53 +39,101 @@ public class Controller implements Initializable {
 	private HBox hBoxRec1, hBoxRec2; 
 	
 	@FXML
-	private AnchorPane AnchorPane1; 
+	private AnchorPane vesselInfoPane; 
 	
 	@FXML
-	private ImageView imgViewInk, imgViewAvg; 
+	private ImageView statusImg, vesselImg; 
 	
 	private ArrayList <String> IDs = new ArrayList<>(Arrays.asList("IMO:9398917", "IMO:9371878", "IMO:9299707", "IMO:9425356", "IMO:9186728", "IMO:9057173", "IMO:9247168"));
 	private PortCDMApi portcdmApi;
+	private Map<String, PortCallSummary> portCallTable;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {	
 		boolean useDevServer = true;
 		portcdmApi = new PortCDMApi(useDevServer);
-		getAndSend();
-		
-		ObservableList<Button> buttons = FXCollections.observableArrayList();
-		for (int i = 0; i < IDs.size(); i++) {
-			Button b = new Button();
-			b.setText(IDs.get(i));
-			buttons.add(b);
-			b.setOnAction((event) -> { popWindow(b); });	
-		}
-		IDListView.setItems(buttons);	
+		portCallTable = createPortCallTable(10);
+		populateIdList();
 	}
 	
 	/**
-	 * DOCUMENTATION HERE PLEASE!
+	 * Creates a hashmap of portcall summaries fetched from PortCDM.
+	 * Each summary is index by its IMO. 
 	 * 
-	 * @param b description of parameter
+	 * @param max maximum number of summaries to fetch
+	 * @return hashmap with the fetched portcall summaries
 	 */
-	private void popWindow(Button b) {
-		String id = b.getText(); 
-		AnchorPane1.setVisible(true);
+	private HashMap<String, PortCallSummary> createPortCallTable(int max) {
+		HashMap<String, PortCallSummary> map = new HashMap<>();
+		List<PortCallSummary> summaries = portcdmApi.getPortCalls(max);
+		for (PortCallSummary s : summaries) {
+			map.put(s.getVessel().getImo(), s);
+		}
+		return map;
+	}
+	
+	/**
+	 * Populates the idListView with ids found in portCallTable.
+	 */	
+	private void populateIdList() {
+		ObservableList<String> listElements = FXCollections.observableArrayList(portCallTable.keySet());
+		idListView.setItems(listElements); // What if listElements is empty?
+	}
+	
+	/**
+	 * This method handles mouse clicks from the idListView.
+	 * It makes a new panel visible containing information about the vessel corresponding to the clicked id.
+	 * 
+	 * @param e mouse event with information about the clicked element
+	 */	
+	@FXML
+	public void handleMouseClick(MouseEvent event) {
+		String id = idListView.getSelectionModel().getSelectedItem();
 		idText.setText(id);
-		bookTimeText.setText("17:25 23-april"); //TODO: make interactive
-		etaTimeText.setText("03:12 24-april");
+		vesselInfoPane.setVisible(true);
 		
-		if (id.equals("IMO:9371878")) { //TODO: change the image in some other way
-			imgViewAvg.setVisible(false);
-			imgViewInk.setVisible(true);
-			inkOrAvgText.setText("Inkommande");
+		// Get the portcall summary corresponding to the list element,
+		// and update the information in the vesselInfoPane. 
+		PortCallSummary summary = portCallTable.get(id);
+		bookTimeText.setText(summary.getCreatedAt().substring(10));
+		etaTimeText.setText(summary.getLastUpdate().substring(10));
+		
+		// Download the image of the vessel and adjust its size
+		String photoUrl = summary.getVessel().getPhotoURL();
+		Image vesselPhoto = downloadImage(photoUrl);
+		vesselImg.setImage(vesselPhoto);
+		vesselImg.setFitWidth(300);
+		vesselImg.setFitHeight(200);
+		
+		// Set all the portcalls with an IMO starting with "7" to incoming
+		if (id.startsWith("7")) {
+			statusImg.setImage(new Image("pilotapplication/img/Inkommande.png"));
+			vesselStatusText.setText("Inkommande");
 		}	
 		else {
-			imgViewAvg.setVisible(true); 
-			imgViewInk.setVisible(false);
-			inkOrAvgText.setText("Avgående");
+			statusImg.setImage(new Image("pilotapplication/img/Avgående.png"));
+			vesselStatusText.setText("Avgående");
 		}
 	}
+	
+	/**
+	 * Download image from URL.
+	 * 
+	 * @param url string representation of the URL pointing to the image
+	 * @return the downloaded image if no exceptions were caught
+	 */
+	private Image downloadImage(String url) {
+		Image img = null;
+		try {
+			URLConnection conn = new URL(url).openConnection();
+			conn.setRequestProperty("User-Agent", "Wget/1.13.4 (linux-gnu)");
+			InputStream in = conn.getInputStream();
+			img = new Image(in);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return img;
+	}	
 	
 	/**
 	 * Simple method for testing the api to portcdm
