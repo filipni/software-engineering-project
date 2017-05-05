a729c30ec7ff26ebd17198d9bf84f1b4f927e406
filipni <filipni@student.chalmers.se>
2017-05-05 12:25:24 +0200

    Removed unnecessary imports.

--- pilotApplication/src/main/java/pilotapplication/Controller.java
+++ pilotApplication/src/main/java/pilotapplication/Controller.java
@@ -1,8 +1,6 @@
 package pilotapplication;
 
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
